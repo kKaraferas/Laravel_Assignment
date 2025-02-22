9ea73fce77a156706c0e708baa57c2693db2f88b
Kostas Karaferas <k.karaferas@hotmail.gr>
2025-02-23 00:15:33 +0200

    User registration,log-in,log-out

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Dotenv\Exception\ValidationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function registerUser(Request $request){
+        try{
+            //Validate the registration
+            $request->validate([
+                'name'=> 'required|string|max:50',
+                'email'=> 'required|string|email|unique:users',
+                'password'=>'required|min:8'
+            ]);
+
+            // if ($request->fails()){
+            //     return response()->json([
+            //         'status' => false,
+            //         'message' => 'Validation error',
+                    
+            //     ], 401);
+            // }
+            
+            //User Creation
+            $user = User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>Hash::make($request->password),
+            ]);
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'user'=>$user,
+                'message'=>'User Creation Successful',
+                'token'=>$token
+            ]);
+        }
+        catch(\Throwable $th){
+            return 0;
+        }
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+
+        ///////check here maybe one out
+
+        // if(!$user || !Hash::check($request->password, $user->password)){
+        //     return response()->json([
+        //         'status' => false,
+        //         'message' => 'Validation error',
+        //         'errors' => $request->errors(),
+        //     ]);
+        // }
+
+        if(!Auth::attempt($request->only(['email', 'password']))){
+            return response()->json([
+                'status' => false,
+                'message' => 'Incorrect Credentials',
+            ]);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        //////////
+
+        $token = $user -> createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+            'message' => 'User logged in.'
+        ]);
+    }
+
+    public function logOut(Request $request){
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged Out.'
+        ]);
+    }
+}
